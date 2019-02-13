4e59c293ca640d3fb67b727dbb66886e0c542093
Mikhail Khimukhin <khimukhin@ians.aero>
2019-02-13 10:08:06 +0300

    window created

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from MWidgets import Window
+from MWidgets import Scene
+from MWidgets import PygameGUI
 
 if __name__ == "__main__":
-    print("!")
+    win = Window((0, 0, 200, 100), layout_name="game")
+    
+    scn = Scene((0, 0, 1, 1), 20, 10)
+    win.add_child("game", scn)
+    
+    gui = PygameGUI(200, 100)
+    win.set_gui(gui)
+    
+    win.exec()
