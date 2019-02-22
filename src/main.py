fed0f39f5db595ab894b58776876a607dcd93fd7
Mikhail Khimukhin <khimukhin@ians.aero>
2019-02-22 16:57:41 +0300

    window optimized

--- src/main.py
+++ src/main.py
@@ -12,17 +12,15 @@ def play_tetris():
     cell_size = 20
     speed = 20
     w, h = 10, 20
-    win = Window((0, 0, w*cell_size, h*cell_size), layout_name="game")
+    gui = PygameGUI(w*cell_size, h*cell_size)
+    win = Window(gui, layout_name="game")
 
     scn = GameScene((0, 0, 1, 1), cell_size, speed, win)
     scn.set_trigger(Event.Key.K_LEFT, win, "move_left")
     scn.set_trigger(Event.Key.K_RIGHT, win, "move_right")
     scn.set_trigger(Event.Key.K_UP, win, "rotate_left")
     scn.set_trigger(Event.Key.K_DOWN, win, "drop")
-    win.add_child("game", scn)
-
-    gui = PygameGUI(w*cell_size, h*cell_size)
-    win.set_gui(gui)
+    win.add_child(scn, "game")
 
     win.exec()
 
