c554670435edc2aa8515eebcc330e4824a902544
GreenBlackSky <greenblacksky@gmail.com>
2019-05-01 19:07:17 +0300

    Drawing optimized

--- src/gamescene.py
+++ src/gamescene.py
@@ -13,14 +13,23 @@ class GameScene(Canvas):
         super().__init__(master, **kargs)
         self._width = 10
         self._height = 20
-        self._scale = 30
+        self._cell_size = 30
         self._step = 100
 
         self.config(
-            width=(self._width*self._scale),
-            height=(self._height*self._scale),
+            width=(self._width*self._cell_size),
+            height=(self._height*self._cell_size),
             background='white'
         )
+
+        for y in range(self._height):
+            for x in range(self._width):
+                self.create_rectangle(
+                    x*self._cell_size,
+                    y*self._cell_size,
+                    x*self._cell_size + self._cell_size,
+                    y*self._cell_size + self._cell_size
+                )
 
         self._game = Tetris(1, self._width, self._height)
         self._run = False
@@ -32,22 +41,33 @@ class GameScene(Canvas):
             return
 
         self._game.update()
-        self.clear()
+        self._clear()
+        self._draw_figure()
+        self._draw_filled_cells()
+        self.after(self._step, self.update)
+
+    def _clear(self):
+        for item in self.find_all():
+            self.itemconfig(item, fill='white')
+
+    def _draw_figure(self):
         fig_x, fig_y = self._game.current_figure.x, self._game.current_figure.y
         for x, y in self._game.current_figure:
-            self._draw_rect(fig_x + x, fig_y + y)
+            item = self.find_closest(
+                (fig_x + x + 0.5)*self._cell_size,
+                (fig_y + y + 0.5)*self._cell_size
+            )
+            self.itemconfig(item, fill='green')
+
+    def _draw_filled_cells(self):
         for y, line in enumerate(self._game.field):
             for x, val in enumerate(line):
                 if val:
-                    self._draw_rect(x, y)
-        self.after(self._step, self.update)
-
-    def clear(self):
-        self.delete('all')
-
-    def _draw_rect(self, x, y):
-        args = [arg*self._scale for arg in [x, y, x + 1, y + 1]]
-        self.create_rectangle(*args, fill='green')
+                    item = self.find_closest(
+                        (x + 0.5)*self._cell_size,
+                        (y + 0.5)*self._cell_size
+                    )
+                    self.itemconfig(item, fill='green')
 
     @property
     def run(self):
